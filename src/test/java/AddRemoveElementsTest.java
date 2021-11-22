2cdffad0be1c80e6ff065b35cf15aa662ed3dcb1
DmitryKhilko <dmitrykhilko@mail.ru>
2021-11-22 23:57:37 +0300

    Initial commit

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class AddRemoveElementsTest extends BaseTest{
+
+    @Test
+    public void addRemoveElements(){
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement cmdAddElement = driver.findElement(By.xpath("//button[@onclick = 'addElement()']"));
+        cmdAddElement.click();
+        cmdAddElement.click();
+        List<WebElement> cmdDelete = driver.findElements(By.xpath("//button[@onclick = 'deleteElement()']"));
+        Assert.assertEquals(cmdDelete.size(),2,"Количество элементов не равно 2");
+
+        cmdDelete.get(1).click();
+        List<WebElement> cmdDelete1 = driver.findElements(By.xpath("//button[@onclick = 'deleteElement()']"));
+        Assert.assertEquals(cmdDelete1.size(),1,"Количество элементов не равно 1");
+    }
+}
